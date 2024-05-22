e69fd6e0b2034b5f7aee8571a1beb8fce66dbca9
Muhammad Angga Muttaqien <muha.muttaqien@gmail.com>
2024-05-22 12:45:00 +0900

    Added ROS 2.

---
+++ 2. Udacity Robotics/ROS 2 Workspace/ros2_ws/build/my_robot_controller/share/my_robot_controller/hook/pythonpath_develop.ps1
@@ -0,0 +1,3 @@
+# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
+
+colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/muttaqien-m/ros2_ws/build/my_robot_controller"
